53159aae34b8d5a9180af8b2a39fe0e111433e4c
wahyuumaternate <retmujago@gmail.com>
2025-12-03 07:57:16 +0900

    cors

---
+++ config/cors.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        '*', // Untuk development - izinkan semua origin
+        // Untuk production, specify domain Anda:
+        'https://satudata.ternatekota.go.id',
+        'https://pala.ternatekota.go.id',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
